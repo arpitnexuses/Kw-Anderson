c9322e3c919f8d1b32eb2b2ee6c14c1721fd878c
arpitnexuses <arpit.m@nexuses.in>
2024-12-24 17:15:59 +0530

    commit 1

---
+++ countries.js
@@ -0,0 +1,9 @@
+window.countriesData = {
+    'countries': [
+        {
+            'name': 'Albania',
+            'link': 'https://global.andersen.com/locations2/#AL'
+        },
+        // ... rest of your countries data
+    ]
+}; 
---
+++ united_states_offices.js
@@ -0,0 +1,12 @@
+window.us_offices = [
+    {
+        "title": "New York, NY",
+        "streetAddress": "1177 Avenue of the Americas<br>18th Floor",
+        "city": "New York",
+        "state": "NY",
+        "country": "US",
+        "postalCode": "10036",
+        // ... rest of the office data
+    },
+    // ... more offices
+]; 
